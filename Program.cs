e5f81f00f8d94ef171173c79b8c632456af1c28b
weilaoshi527 <weilaoshi527@qq.com>
2016-12-22 15:26:46 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: abcde
+ * Date: 2016/12/22
+ * Time: 14:48
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace ProbabTest
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine(Probability.re_t(1,0.975).ToString());
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
